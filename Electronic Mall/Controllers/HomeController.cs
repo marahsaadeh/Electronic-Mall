934bf7bdfcf20dbe1840ea4f2795148ad8fa3660
marahsaadeh <marah4saadeh@gmail.com>
2024-04-19 23:36:22 +0200

    Editing of files in the user-authentication branch

--- Electronic Mall/Controllers/HomeController.cs
+++ Electronic Mall/Controllers/HomeController.cs
@@ -30,7 +30,13 @@ namespace Electronic_Mall.Controllers
             return View(cats);
       
         }
-       
+<<<<<<< HEAD
+
+        //localhost:7171/Home/SubCategory?subCategoryId={id}
+
+=======
+        //localhost:7171/Home/SubCategory?subCategoryId={id}
+>>>>>>> 2965ca98be0b149daf1e0d1a8ab8c08b58993e45
         public ActionResult SubCategory(int subCategoryId)
         {
             ApplicationDbContext db = new ApplicationDbContext();
